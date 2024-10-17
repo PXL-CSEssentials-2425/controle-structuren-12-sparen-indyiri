173f6576e3c5f2b7b1ccedf8b2f24f148a2cae8c
Indy Iri <indy.iri@student.pxl.be>
2024-10-17 16:37:12 +0200

    Afgewerkt? Onzeker of deze manier aanvaardbaar is

--- H3Oef12Sparen/H3Oef12Sparen/MainWindow.xaml.cs
+++ H3Oef12Sparen/H3Oef12Sparen/MainWindow.xaml.cs
@@ -36,7 +36,43 @@ namespace H3Oef12Sparen
 
         private void calculateButton_Click(object sender, RoutedEventArgs e)
         {
+            string inputAllowance = allowanceTextBox.Text;
+            string inputWeeklyRaise = weeklyRaiseTextBox.Text;
+            string inputDesiredSavingsAmount = desiredSavingsAmountTextBox.Text;
 
+            double allowance;
+            double weeklyRaise;
+            double desiredSavingsAmount;
+            int amountOfWeeks = 0;
+            double savingsAtThatMoment = 0;
+            double extraAllowance = 0;
+
+            bool isInputAllowanceValid = double.TryParse(inputAllowance, out allowance);
+            bool isInputWeeklyRaiseValid = double.TryParse(inputWeeklyRaise, out weeklyRaise);
+            bool isInputDesiredSavingsAmountValid = double.TryParse(inputDesiredSavingsAmount, out desiredSavingsAmount);
+
+            if (isInputAllowanceValid || isInputWeeklyRaiseValid || isInputDesiredSavingsAmountValid)
+            {
+                while (savingsAtThatMoment < desiredSavingsAmount)
+                {
+                    amountOfWeeks++;
+                    savingsAtThatMoment = amountOfWeeks * allowance;
+                    extraAllowance = (amountOfWeeks - 1 ) * weeklyRaise;
+                    
+                    //savingsAtThatMoment += (allowance * (weeklyRaise + 1));                   
+                    
+
+                    //allowance = (allowance * (weeklyRaise + 1));
+                    //savingsAtThatMoment += allowance;
+                    //amountOfWeeks++;
+
+                }               
+                resultTextBox.Text = $"Spaarbedrag na {amountOfWeeks - 1} weken: €{Math.Round(savingsAtThatMoment - allowance,2)} \n\rExtra weekgeld op dat moment: €{Math.Round(extraAllowance, 2)} \n\rTotaal spaargeld: €{Math.Round((savingsAtThatMoment - allowance) + extraAllowance, 2)} ";                
+            }
+            else
+            {
+                resultTextBox.Text = "Geef correcte waarden in!";
+            }
         }
     }
 }
